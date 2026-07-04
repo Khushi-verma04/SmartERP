275f4d4fb3351fc0f3996f03632f8a97836c4b23
Khushi Verma <mm0981389@gmail.com>
2026-07-04 18:03:36 +0530

    show database connection error

--- backend/index.js
+++ backend/index.js
@@ -1,47 +1,11 @@
-const cors = require("cors");
 const express = require("express");
-const pool = require("./db");
-const authRoutes = require("./routes/authRoutes");
-const companyRoutes = require("./routes/companyRoutes");
-const ledgerRoutes = require("./routes/ledgerRoutes");
-const groupRoutes = require("./routes/groupRoutes");
-const stockRoutes = require("./routes/stockRoutes");
-const purchaseRoutes = require("./routes/purchaseRoutes");
-const supplierRoutes = require("./routes/supplierRoutes");
-const salesRoutes = require("./routes/salesRoutes");
-const customerRoutes = require("./routes/customerRoutes");
-const reportRoutes = require("./routes/reportRoutes");
-const inventoryRoutes = require("./routes/inventoryRoutes");
+const cors = require("cors");
 
 const app = express();
-
-app.use(express.json());
 app.use(cors());
 
-app.use("/api/auth", authRoutes);
-app.use("/api/company", companyRoutes);
-app.use("/api/ledger", ledgerRoutes);
-app.use("/api/group", groupRoutes);
-app.use("/api/stock", stockRoutes);
-app.use("/api/purchase", purchaseRoutes);
-app.use("/api/supplier", supplierRoutes);
-app.use("/api/sales", salesRoutes);
-app.use("/api/customer", customerRoutes);
-app.use("/api/reports", reportRoutes);
-app.use("/api/inventory", inventoryRoutes);
-app.use("/api/dashboard", require("./routes/dashboardRoutes"));
-
-app.get("/", async (req, res) => {
-  try {
-    const result = await pool.query("SELECT NOW()");
-    res.send(`SmartERP Backend Running. Database Connected at ${result.rows[0].now}`);
- } catch (error) {
-  console.error("Database Error:", error);
-  res.status(500).json({
-    message: "Database Connection Failed",
-    error: error.message,
-  });
-}
+app.get("/", (req, res) => {
+  res.send("Backend Running");
 });
 
 const PORT = process.env.PORT || 5000;
